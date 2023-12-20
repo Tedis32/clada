09261b1137a4afd6d17ff086b722bbe6f1318918
TedisStumbrs <teddrinkscoffee@gmail.com>
2023-12-20 13:47:09 +0000

    Revert "Removed app directory out"
    This reverts commit 20e45ddbda502f2e7c939d47a85e614dad8e47b0.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("70389ebdc6ad3c50")
+/******/ 		__webpack_require__.h = () => ("6d5ad4cf504fefa4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\User\Desktop\clada-dev-service\clada\src\app\layout.js
+// File: C:\Users\User\Desktop\clada-dev-service\clada\src\app\layout.jsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
